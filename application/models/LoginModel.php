21ccf84d9fd851523972ba2c4a22c2c4638545f5
Geavannyef <geavannyef@gmail.com>
2017-10-22 17:14:41 +0700

    Update LoginModel
    ngapus method getDate() karna uda tidak berguna :"

--- application/models/LoginModel.php
+++ application/models/LoginModel.php
@@ -12,17 +12,6 @@ class LoginModel extends CI_Model {
 		return $query;
 	}
 
-	function getData($username, $password)
-	{
-		$this->db->select('*');
-		$this->db->where('username', $username); //ngecel apakah usernamenya ada di database
-		$this->db->where('password', $password); 
-		$this->db->where('role = 0');
-		$this->db->from('akun');
-		$query = $this->db->get();
-		return $query->result_array();
-	}
-
 	function addAcc($data){
 		$this->db->insert('akun', $data);
 	}
